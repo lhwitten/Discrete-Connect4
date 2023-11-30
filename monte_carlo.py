decfde4f1e7d0ad9525766fa29dc8e91d6c82b67
Allyson Hur <allysonhur@gmail.com>
2023-11-30 14:17:10 -0500

    Update monte_carlo.py

--- monte_carlo.py
+++ monte_carlo.py
@@ -2,6 +2,9 @@
 
 # Set root node to the current game state
 import math
+import time
+import random
+from copy import deepcopy
 
 class Node:
 
@@ -13,35 +16,126 @@ class Node:
     def __init__(self,game_state,parent):
         self.game_state = game_state        # matrix
         self.children = {}
-        self.visits = 0
-        self.wins = 0      # Total games won / total games played OR Q/N
-        self.UCB = (self.wins / self.visits) + self.exploration_value * math.sqrt(math.ln(self.visits)/self.wins)
-
-    
+        self.total_played = 0     # N
+        self.total_won = 0      # Q
+        self.UCB = (self.total_won / self.total_played) + self.exploration_value * math.sqrt(math.ln(self.total_played)/self.total_won)
 
     
     def add_children(self,children:dict):
         # Include all possible unexplored children connected to current node
         for child in children:
-            self.children[child.state] = child 
+            self.children[child.game_state] = child 
+    def calculate_UCB():
+            pass
+    
 
-    def selection(self, root_node):
+    #def monte_carlo_search()
+class MonteCarlo:
+    def __init__(self, state=ConnectState()):
+        self.root_state = deepcopy(state)
+        self.root = Node(None,None)
+        self.run_time = 0
+        self.node_count = 0
+        self.num_rollouts = 0
+
+    
+
+    def selection(self) -> tuple:
         # return best child node
         # select node with the highest UCB
+        node = self.root
+        state = deepcopy(self.root_state)
+
+        while len(node.children) != 0:
+            children = node.children.values()
+            max_value = max(children, key=lambda n: n.value()).value()
+            max_nodes = [n for n in children if n.value() == max_value]
+
+            node = random.choice(max_nodes)
+            state.move(node.move)
+
+            if node.total_played == 0:
+                return node, state
+        
+        if self.expansion(node,state):
+            node = random.choice(list(node.children.values()))
+            state.move(node.move)
+        return node,state
+
+
         
         # select a leaf node that has not been explored OR according to UCB
         pass
-    def expansion(self, children :dict):
-          
-        pass
-    def simulation(self):
-        # Select child node (can be random) and play it out until it reaches final stage of game (keeping in mind legalities of moves)
-        pass
-    def backpropagation(self):
-        # When final state with a winner is reached, all traversed nodes are updated (visit and win-value)
-        pass
-    # The child of the root node with the highest number of visits is selected as the next move
+    def expansion(self, parent: Node, state: ConnectState) -> bool: 
+        if state.game_over():
+            return False
+        children = [Node(move,parent) for move in state.get_legal_moves()]
+        parent.add_children(children)
 
-    #def monte_carlo_search()
+        return True
+    
+    def simulation(self, state: ConnectState) -> int:
+        # Select child node (can be random) and play it out until it reaches final stage of game (keeping in mind legalities of moves)
+        while not state.game_over():
+            state.move(random.choice(state.get_legal_moves()))
+
+        return state.get_outcome()
+    
+    def backpropagation(self, node: Node, turn: int, outcome: int) -> None:
+        reward = 0 if outcome == turn else 1
+
+        while node is not None:
+            node.total_played +=1
+            node.total_wins += reward
+            node = node.part
+            if outcome == GameMeta.OUTCOMES['draw']:
+                reward = 0
+            else:
+                reward = 1 - reward
+        # When final state with a winner is reached, all traversed nodes are updated (visit and win-value)
+    # The child of the root node with the highest number of total_played is selected as the next move
+
+    def search(self, time_limit: int):
+        start_time = time.process_time()
+
+        num_rollouts = 0
+
+        while time.process_time() - start_time < time_limit:
+            node, state = self.select_node()
+            outcome = self.simulation(state)
+            self.backpropagation(node, state.to_play, outcome)
+            num_rollouts += 1
         
+        run_time = time.process_time() - start_time
+        self.run_time = run_time
+        self.num_rollouts = num_rollouts
+
+    
+    def best_move(self):
+        if self.root_state.game_over():
+            return -1
+        max_value = max(self.root.children.values(), key=lambda n: n.total_played).total_played
+        max_nodes = [n for n in self.root.children.values() if n.total_played == max_value]
+        best_child = random.choice(max_nodes)
+
+        return best_child.move
+    
+
+    def move(self, move):
+        if move in self.root.children:
+            self.root_state.move(move)
+            self.root = self.root.children[move]
+            return
+        
+        self.root_state.move(move)
+        self.root = Node(None, None)
+
+
+    def statistics(self) -> tuple:
+        return self.num_rollouts, self.run_time
+
+
+
+
+    
 
