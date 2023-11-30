0618451f523cdbff826738bf621ef170a73616cd
Allyson Hur <allysonhur@gmail.com>
2023-11-30 14:17:44 -0500

    Create game_rules.py

---
+++ game_rules.py
@@ -0,0 +1,8 @@
+import math
+
+class GameSetup:
+    PLAYERS = {'none': 0, 'one': 1, 'two': 2}
+    OUTCOMES = {'none': 0, 'one': 1, 'two': 2, 'draw': 3}
+    INF = float('inf')
+    ROWS = 6
+    COLS = 7
