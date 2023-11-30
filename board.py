e92e671f34f7a2a0e1a4470b44bdad533a9ae030
Allyson Hur <allysonhur@gmail.com>
2023-11-30 14:16:42 -0500

    Update board.py

--- board.py
+++ board.py
@@ -1,26 +1,32 @@
 import numpy as np
 
-ROW_COUNT = 6
-COLUMN_COUNT = 7
+ROWS = 6
+COLUMNS = 7
 
 # Matrix of 7x7 zeros. Zero represents empty (not yet played).
 
-def initialize_board():
-    board = np.zeros((ROW_COUNT, COLUMN_COUNT))
+def create_board():
+    board = np.zeros((ROWS,COLUMNS))
     return board
-
+ 
+def update_board(board, row, col, piece):
+    board[row][col] = piece
+ 
 def is_legal_move(board, col):
-    row_number = 0
-    for row in board:
-        if board[row][col] == 0:
-            row_number = row
-            break
-    return row_number
+    # returns true if space is empty
+    for row, col in board:
+        if row == 0 and board[row][col] == 0:
+            return board[row][col] == 0
+ 
+def next_valid_row(board, col):
+    for r in range(ROWS):
+        if board[r][col] == 0:
+            return r
+        
+def check_win():
+    pass
+        
 
-def update_board(board, row, col):
-    if is_legal_move(board, col):
-        board[row][col] = 1
-    return board
 
     
 
