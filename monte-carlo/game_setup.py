fdb5735d446187fb492e3bae540cb4c5cd517bc9
allyson-hur <allysonhur@gmail.com>
2023-12-09 15:46:48 -0500

    Update game_setup documentation

--- monte-carlo/game_setup.py
+++ monte-carlo/game_setup.py
@@ -6,14 +6,6 @@ import math
 class GameSetup:
     """
     Sets up important components of the Connect 4 game.
-    
-    Attributes:
-        PLAYERS: dictionary for players.
-        OUTCOMES: dictionary for outcomes of game.
-        EXPLORATION: a heuristic value used to calculate UCB.
-        INF: infinity
-        ROWS: number of rows on the board
-        COLS: number of columns on the board.
     """
     PLAYERS = {'none': 0, 'one': 1, 'two': 2}
     OUTCOMES = {'none': 0, 'one': 1, 'two': 2, 'draw': 3}
